152d47698306cbf8e88ceff4579027a64c1c396d
Nuno Ferreira <loferreira.nuno@gmail.com>
2021-07-01 19:44:12 +0200

    greeter

--- src/Services/GreeterService.cs
+++ src/Services/GreeterService.cs
@@ -7,17 +7,20 @@ namespace src
     public class GreeterService : Greeter.GreeterBase
     {
         private readonly ILogger<GreeterService> _logger;
+
         public GreeterService(ILogger<GreeterService> logger)
         {
             _logger = logger;
-        }  
+        }
 
-        public Task<HelloReply> SayHello(HelloRequest request)
+        public override Task<HelloReply> SayHello(HelloRequest request,
+            ServerCallContext context)
         {
-            var reply = new HelloReply{
-                Message = $"Hello {request.Name}"
-            };
-            return Task.FromResult(reply);
+            _logger.LogInformation("Saying hello to {Name}", request.Name);
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
         }
     }
 }
