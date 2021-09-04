5e3d2bd8c725ec83e0ddc1120f5a4634dab20155
loferreiranuno <loferreira.nuno@gmail.com>
2021-09-04 09:03:19 +0200

    5300

--- src/Program.cs
+++ src/Program.cs
@@ -19,8 +19,8 @@ namespace FoodTrack.Database
                 .ConfigureWebHostDefaults(webBuilder =>
                 { 
                     webBuilder.UseKestrel(options => {
-                        options.Listen(IPAddress.Any, 80, listenOptions => {
-                            listenOptions.Protocols = HttpProtocols.Http2;
+                        options.Listen(IPAddress.Any, 5300, listenOptions => {
+                            listenOptions.Protocols = HttpProtocols.Http2; 
                         });
                     });
                     webBuilder.UseStartup<Startup>();
